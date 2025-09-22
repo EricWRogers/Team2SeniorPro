b735c721a3919b634c7cec3760eeed15caf8d7f1
ldroberts02 <ldroberts02@gmail.com>
2025-09-22 17:11:17 -0500

    Luke's work to be pushed
    Trigger work, player script work with get component, etc.

--- Assets/WorkFolder/Cristian/Scripts/Movement/ThirdPersonCam.cs
+++ Assets/WorkFolder/Cristian/Scripts/Movement/ThirdPersonCam.cs
@@ -48,6 +48,46 @@ public class ThirdPersonCam : MonoBehaviour
         Cursor.visible = false;
 
         targetDistance = currentDistance = Mathf.Clamp(defaultDistance, minDistance, maxDistance);
+
+        if (player == null | playerObj == null | orientation == null | playerObj == null)
+        {
+            if (GameObject.FindGameObjectWithTag("Player").gameObject.name == "Squirrel")
+            {
+                player = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+            else
+            {
+                Debug.LogError("Cant get player object!");
+            }
+            if (player.transform.GetChild(0).gameObject.name == "PlayerObj")
+            {
+                playerObj = player.transform.GetChild(0).transform;
+            }
+            else
+            {
+                Debug.LogError("Cant get playerObj object!");
+            }
+            if (player.transform.GetChild(1).gameObject.name == "Orientation")
+            {
+                orientation = player.transform.GetChild(1).transform;
+            }
+            else
+            {
+                Debug.LogError("Cant get orientation object!");
+            }
+            if (orientation.transform.GetChild(0).gameObject.name == "ShoulderLookAt")
+            {
+                shoulderLookAt = orientation.transform.GetChild(0).transform;
+            }
+            else
+            {
+                Debug.LogError("Cant get shoulderlookat object!");
+            }
+
+            //playerObj = player.transform.GetChild(0).transform;
+            //orientation = playerObj.transform.GetChild(1).transform;
+            //shoulderLookAt = orientation.transform.GetChild(0).transform;
+        }
     }
 
     void Update()
