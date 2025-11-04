9ce2e57751e67ad3f1b43b0c2f406fffd85dbac3
K.D.W <kadendw23@gmail.com>
2025-11-03 20:22:54 -0600

    Level_1 Cutscene Camera Finished
    - Testing phase complete

---
+++ Assets/Scripts/Cameras/FlyCam2.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.Splines;
+using System.Collections;
+
+public class FlyCam2 : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject player; // Player object with scripts
+    public GameObject playerCanvas; // Player UI Canvas
+    public GameObject playerCam; // Player camera (Main Camera)
+    public GameObject splineCamOBJ; // Spline camera object
+    public SplineAnimate SplineCam; // Spline camera with spline animation component
+    public MonoBehaviour pauseMenu; // Reference to pauseMenu script
+    
+    public bool inSequence = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (playerCanvas != null) playerCanvas.SetActive(false); // Hide at start
+
+        if (splineCamOBJ != null)
+            splineCamOBJ.SetActive(true);
+        
+        StartCoroutine(StartSequence());
+    }
+
+    private IEnumerator StartSequence()
+    {
+        if (inSequence) yield break;
+        inSequence = true;
+
+        // Disable player scripts
+        if (player != null)
+        {
+            foreach (var script in player.GetComponents<MonoBehaviour>())
+                script.enabled = false;
+
+        }
+        
+        // Disable Player Canvas
+        if (playerCanvas != null)
+            playerCanvas.SetActive(false);
+
+        // Disable pause menu
+        if (pauseMenu != null)
+            pauseMenu.enabled = false;
+        
+
+        // Switch cameras
+        if (playerCam != null) playerCam.SetActive(false);
+        if (splineCamOBJ != null) splineCamOBJ.SetActive(true);
+
+        // Play spline forward and wait til finished
+        if (SplineCam != null)
+        {
+            SplineCam.NormalizedTime = 0f;
+            SplineCam.Play();
+
+            yield return new WaitUntil(() => !SplineCam.IsPlaying);
+            SplineCam.StopAllCoroutines();
+
+        }
+
+        // Unlock mouse and show cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        EndSequence();
+    }
+
+    private void EndSequence()
+    {
+        // Switch cameras back
+        if (playerCam != null) playerCam.SetActive(true);
+        if (splineCamOBJ != null) splineCamOBJ.SetActive(false);
+
+        // Re-enable player scripts
+        if (player != null)
+        {
+            foreach (var script in player.GetComponents<MonoBehaviour>())
+                script.enabled = true;
+        }
+
+        // Disable Player Canvas
+        if (playerCanvas != null)
+            playerCanvas.SetActive(true);
+
+        // Re-enable pause menu
+        if (pauseMenu != null)
+            pauseMenu.enabled = true;
+
+        inSequence = false;
+    }
+}
