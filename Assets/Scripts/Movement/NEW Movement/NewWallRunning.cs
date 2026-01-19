64c3c0b0ff2d5c60bd73ca5691b5dafc35ab4918
Cristian Nalls <cristiannalls@gmail.com>
2026-01-19 14:46:11 -0600

    Closer to finished

--- Assets/Scripts/Movement/NEW Movement/NewWallRunning.cs
+++ Assets/Scripts/Movement/NEW Movement/NewWallRunning.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WNewWallRunning : MonoBehaviour
+public class NewWallRunning : MonoBehaviour
 {
     [Header("Wallrunning")]
     public LayerMask whatIsWall;
