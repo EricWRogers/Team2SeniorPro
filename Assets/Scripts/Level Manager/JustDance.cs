4fcc68825d69cd7ccce743d2b14537955578fdc2
K.D.W <kadendw23@gmail.com>
2026-01-28 13:53:54 -0600

    Dancing Squirrel 90% Done
    - Layout is finished, just need to get the 2nd half completed when the player wins

--- Assets/Scripts/Level Manager/JustDance.cs
+++ Assets/Scripts/Level Manager/JustDance.cs
@@ -18,8 +18,7 @@ public class JustDance : MonoBehaviour
 
     void Update()
     {
-        // Check for user input (e.g., pressing the space bar) to trigger the dance
-        if (Input.GetKeyDown(KeyCode.Space) && playerAnimator != null)
+        if (playerAnimator != null)
         {
             StartTheDance();
         }
