5e37cd52a12e08a30a2aeb96a049acc1597bc647
CNalls <cristiannalls@gmail.com>
2026-03-03 17:07:24 -0600

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Movement/NEW Movement/NewThirdPersonCam.cs
+++ Assets/Scripts/Movement/NEW Movement/NewThirdPersonCam.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewThirdPersonCam : MonoBehaviour
 {
@@ -25,6 +26,33 @@ public class NewThirdPersonCam : MonoBehaviour
         Combat,
         Topdown
     }
+    
+    private PlayerControlsB controls;
+    private Vector2 moveInput;  // left stick / WASD direction
+
+    private void Awake()
+    {
+        controls = new PlayerControlsB();
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Move.performed += OnMove;
+        controls.Player.Move.canceled += OnMove;
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Move.performed -= OnMove;
+        controls.Player.Move.canceled -= OnMove;
+        controls.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+    }
 
     private void Start()
     {
@@ -39,22 +67,31 @@ public class NewThirdPersonCam : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchCameraStyle(CameraStyle.Combat);
         if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCameraStyle(CameraStyle.Topdown);
 
-        // rotate orientation
+        // rotate orientation to always face the player (used in movement calculations)
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        // roate player object
-        if(currentStyle == CameraStyle.Basic || currentStyle == CameraStyle.Topdown)
+        // rotate player object based on left stick / WASD movement direction
+        if (currentStyle == CameraStyle.Basic || currentStyle == CameraStyle.Topdown)
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+            Vector3 inputDir = Vector3.zero;
+
+            // prefer new input system value if available
+            if (moveInput.sqrMagnitude > 0.001f)
+            {
+                inputDir = orientation.forward * moveInput.y + orientation.right * moveInput.x;
+            }
+            else
+            {
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
+                inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+            }
 
             if (inputDir != Vector3.zero)
                 playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
-
-        else if(currentStyle == CameraStyle.Combat)
+        else if (currentStyle == CameraStyle.Combat)
         {
             Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
             orientation.forward = dirToCombatLookAt.normalized;
