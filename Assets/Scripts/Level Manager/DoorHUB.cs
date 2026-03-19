cce50a21ac79cf7e11eb6b9f33b5789ea93ce459
K.D.W <kadendw23@gmail.com>
2026-03-19 17:59:44 -0500

    Added the Door Nut For Level 5
    - Walking near the nut in front of the door activates UI for total berries

--- Assets/Scripts/Level Manager/DoorHUB.cs
+++ Assets/Scripts/Level Manager/DoorHUB.cs
@@ -1,17 +1,52 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class DoorHUB : MonoBehaviour
 {
-    // Make sure your player GameObject is tagged as "Player" and the door has a Collider component with "Is Trigger" checked.
-    // This script should be attached to the door GameObject.
+    public GameObject[] NutParts; // Array to hold the nut parts
+    public GameObject NutText;
+    public GameObject NutText2;
+    public GameObject NutPanel;
+
+    private bool playerInRange;
+
+    void Start()
+    {
+        if (NutText && NutText2 != null )
+            NutText.SetActive(false);
+            NutText2.SetActive(false);
+        
+        if (NutPanel != null)
+        {
+            NutPanel.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+
+        if (playerInRange){
+            NutPanel.SetActive(true);
+            NutText.SetActive(true);
+        }
+        else{
+            NutPanel.SetActive(false);
+            NutText.SetActive(false);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the colliding object is the player
+        if (other.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
         if (other.CompareTag("Player"))
         {
-            // Load the next scene (make sure to add the scene to the build settings)
-            GameManager.Instance.newMap("Level_1");
+            playerInRange = false;
         }
     }
 }
