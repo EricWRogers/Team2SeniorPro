9dcdbd80d3ec637db29819fd0a3ae7ec9d718c6a
K.D.W <kadendw23@gmail.com>
2026-03-30 12:08:56 -0500

    Fixed Killboxes & Added Tags
    Killbox script now checks for the GM, Death Screen, & Death Countdown Text.
    
    Death Screen & Death Countdown TXT have their own tags for the killbox to check for

--- Assets/Scripts/Level Manager/KillVolume.cs
+++ Assets/Scripts/Level Manager/KillVolume.cs
@@ -7,7 +7,7 @@ public class KillVolume : MonoBehaviour
 {
     // public Transform playerRespawn;  // usually same as BottomRespawn
     public GameObject deathScreen;
-    public TMP_Text countdownText; 
+    public TMP_Text deathCountdown; 
     public GameManager GM;
 
     private float countdownTime = 3f; // time in seconds before respawn after death
@@ -17,6 +17,41 @@ public class KillVolume : MonoBehaviour
     [Tooltip("Scripts to disable when paused and enable when resumed")]
     public MonoBehaviour[] scriptsToToggle;
     public GameObject[] objectsToToggle;
+
+    void Start()
+    {
+        if (GM == null)
+        {
+            GM = FindFirstObjectByType<GameManager>();
+            if (GM == null)
+            {
+                Debug.LogError("No GameManager found in scene!");
+            }
+        }
+
+        if (deathScreen == null)
+        {
+            // This finds ALL objects of type Canvas and searches inside them,
+            // even if they are disabled.
+            Canvas[] allCanvases = Resources.FindObjectsOfTypeAll<Canvas>();
+            foreach (Canvas c in allCanvases)
+            {
+                // Search for a child named exactly "Death Screen"
+                Transform t = c.transform.Find("Death Screen");
+                if (t != null)
+                {
+                    deathScreen = t.gameObject;
+                    break;
+                }
+            }
+        }
+
+        if (deathCountdown == null)
+        {
+            // Since the death screen is found, we can look inside for the text
+            deathCountdown = deathScreen.GetComponentInChildren<TMP_Text>(true);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -48,7 +83,7 @@ public class KillVolume : MonoBehaviour
 
         while (timeLeft > 0)
         {
-            countdownText.text = Mathf.Ceil(timeLeft).ToString();
+            deathCountdown.text = Mathf.Ceil(timeLeft).ToString();
             yield return new WaitForSecondsRealtime(1f);
             timeLeft--;
         }
